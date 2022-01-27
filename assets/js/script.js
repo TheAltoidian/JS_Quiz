3a526e2748063f9379125961d71af88bc6288265
Cullen <cullenjcasey@gmail.com>
2022-01-26 18:50:24 -0800

    added all questsions from example gif

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,33 @@
 var timerEl = document.querySelector(".timer");
-var timeLeft = 3;
+var timeLeft = 75;
 let timerGo;
 
 var quizList = [
     {
-        question: "C is correct",
-        answers: ["A", "B", "C", "D"],
-        correct: "C",
+        question: "Commonly used data types DO NOT include:",
+        answers: ["strings", "booleans", "alerts", "numbers"],
+        correct: "alerts",
     },
     {
-        question: "Question 2",
-        answers: ["AA", "AB", "AC", "AD"],
-        correct: "AC",
-    }
+        question: "The condition in an if / else statement is enclosed with _____.",
+        answers: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        correct: "parenthesis",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____.",
+        answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correct: "all of the above",
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: ["commas", "curly brackets", "quotes", "parenthesis"],
+        correct: "quotes",
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        correct: "console.log",
+    },
 ];
 
 var currentIndex = 0;
